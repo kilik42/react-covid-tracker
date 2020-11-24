4204cf82528dff5256f85ca28259be9f865c5816
marvin evins <marvinevins@yahoo.com>
2020-11-23 21:01:07 -0600

    working on api

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const url ='https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+
+    try {
+        const response = await axios.get(url);
+        console.log(response);
+        return response;
+
+    } catch (error){
+
+
+
+    }
+}
